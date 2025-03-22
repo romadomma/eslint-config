aa7c79b19cdaae30d042efcf50047651b2888e71
Roman Kadykov <roma-domma@yandex.ru>
2025-03-23 00:26:57 +0700

    convert index.mjs to .js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import typescriptEslintEslintPlugin from '@typescript-eslint/eslint-plugin';
-import globals from 'globals';
-import tsParser from '@typescript-eslint/parser';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-import js from '@eslint/js';
-import { FlatCompat } from '@eslint/eslintrc';
-import stylistic from '@stylistic/eslint-plugin';
+const typescriptEslintEslintPlugin = require('@typescript-eslint/eslint-plugin');
+const globals = require('globals');
+const tsParser = require('@typescript-eslint/parser');
+const path = require('node:path');
+const { fileURLToPath } = require('node:url');
+const js = require('@eslint/js');
+const { FlatCompat } = require('@eslint/eslintrc');
+const stylistic = require('@stylistic/eslint-plugin');
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
