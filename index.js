3cbec7578dd1a5452d4e00172d00b53b8e5dc40a
Roman Kadykov <roma-domma@yandex.ru>
2025-03-26 11:35:28 +0700

    first prod commit

--- index.js
+++ index.js
@@ -1,91 +1,79 @@
-const typescriptEslintEslintPlugin = require('@typescript-eslint/eslint-plugin');
-const globals = require('globals');
-const tsParser = require('@typescript-eslint/parser');
-const path = require('node:path');
-const { fileURLToPath } = require('node:url');
-const js = require('@eslint/js');
-const { FlatCompat } = require('@eslint/eslintrc');
-const stylistic = require('@stylistic/eslint-plugin');
+import globals from 'globals';
+import stylistic from '@stylistic/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
-
-export default [
-  ...compat.extends('plugin:@typescript-eslint/recommended'),
-  {
-    plugins: {
-      '@typescript-eslint': typescriptEslintEslintPlugin,
-      '@stylistic': stylistic,
-    },
-
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
+export const getRomadommaConfig = ({ parserOptions }) => {
+  return [
+    stylistic.configs.customize({
+      jsx: false,
+      semi: true,
+    }),
+    ...tseslint.config(
+      eslint.configs.recommended,
+      tseslint.configs.recommended,
+    ),
+    {
+      languageOptions: {
+        globals: {
+          ...globals.node,
+          ...globals.jest,
+        },  
+        parser: tsParser,
+        parserOptions: {
+          ...parserOptions,
+          projectService: true,
+        },
       },
-
-      parser: tsParser,
-      ecmaVersion: 5,
-      sourceType: 'module',
-
-      parserOptions: {
-        project: 'tsconfig.json',
-        tsconfigRootDir: '/home/romadomma/work/quizzy-api',
-      },
-    },
-
-    rules: {
-      '@typescript-eslint/interface-name-prefix': 'off',
-      '@typescript-eslint/explicit-function-return-type': 'off',
-      '@typescript-eslint/explicit-module-boundary-types': 'off',
-      '@typescript-eslint/explicit-member-accessibility': [
-        'error', 
-        {
-          overrides: {
-            constructors: 'off',
+      rules: {
+        '@typescript-eslint/interface-name-prefix': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/explicit-member-accessibility': [
+          'error',
+          {
+            overrides: {
+              constructors: 'off',
+            },
           },
-        }
-      ],
-      '@typescript-eslint/no-misused-promises': [
-        'error',
-        {
-          'checksVoidReturn': false,
-        },
-      ],
-      '@typescript-eslint/naming-convention': [
-        'error',
-        {
-          selector: [
-            'variableLike',
-            'memberLike',
-            'method',
-            'property',
-          ],
-          format: ['camelCase'],
-        },
-        {
-          selector: 'parameter',
-          format: ['camelCase'],
-          leadingUnderscore: 'allow',
-        },
-        {
-          selector: [
-            'enumMember',
-            'typeLike',
-          ],
-          format: ['PascalCase'],
-        },
-      ],
-      '@stylistic/no-trailing-spaces': ['error', { 'skipBlankLines': true }],
-      '@stylistic/comma-dangle': ['error', 'always-multiline'],
-      '@stylistic/comma-spacing': ['error', { 'before': false, 'after': true }],
-      '@stylistic/quotes': ['error', 'single'],
-      '@stylistic/indent': ['error', 2],
-    },
-  },
-];
+        ],
+        '@typescript-eslint/no-misused-promises': [
+          'error',
+          {
+            'checksVoidReturn': false,
+          },
+        ],
+        '@typescript-eslint/naming-convention': [
+          'error',
+          {
+            selector: [
+              'variableLike',
+              'memberLike',
+              'method',
+              'property',
+            ],
+            format: ['camelCase'],
+          },
+          {
+            selector: 'parameter',
+            format: ['camelCase'],
+            leadingUnderscore: 'allow',
+          },
+          {
+            selector: [
+              'enumMember',
+              'typeLike',
+            ],
+            format: ['PascalCase'],
+          },
+        ],
+        '@stylistic/no-trailing-spaces': ['error', { 'skipBlankLines': true }],
+        '@stylistic/comma-dangle': ['error', 'always-multiline'],
+        '@stylistic/comma-spacing': ['error', { 'before': false, 'after': true }],
+        '@stylistic/quotes': ['error', 'single'],
+        '@stylistic/indent': ['error', 2],
+      },
+    }
+  ];
+};
